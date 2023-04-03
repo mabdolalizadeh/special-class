8535c6d1ffef5f9df3f28bf4c124a1d40915c5f1
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2023-04-03 17:48:31 +0330

    finish init

--- special-class.py
+++ special-class.py
@@ -1,3 +1,22 @@
+import cv2
+import vlc
+import numpy as np
+
+
 class Special():
-    def __init__():
-        
+
+    
+    def __init__(self, work) :
+        detectFace = cv2.CascadeClassifire("lib/haarcascade_frontalface_default.xml")
+        detectEye = cv2.CascadeClassifire("lib/haarcascade_eye.xml")
+
+        if work == "eye":
+            self.camera()
+        else
+            self.media(work)
+
+    
+    def camera(self):
+        pass
+    def media(self,work):
+        pass
